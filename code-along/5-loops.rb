de9db27c710469a5cf62e7a52ca85e3b35d5742e
Kajalkaran Shah <drkajal.sh@gmail.com>
2025-01-28 21:37:20 +0000

    5- loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -1,11 +1,22 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
-
+index =0
 # Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
-# end
+loop do
+    if index==5
+        break
+    end
+
+    puts "tacos!"
+
+    index = index + 1
+end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+for taco in tacos
+    puts "#{taco} tacos!"
+    # do something with tacos
+end
