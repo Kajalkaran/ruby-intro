06422ac6e9569460ca0de643ad8dd5ceec726bd8
Kajalkaran Shah <drkajal.sh@gmail.com>
2025-01-28 21:19:28 +0000

    4-hashes file

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,23 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
-
+me = {"name" => "Ben", "location" => "Chicago", "status" => "ENTR-924 at Kellogg"}
+puts me
+p me
 # Accessing data from the hash
-
+location = me["location"]
+puts location
 # More Complex Hashes
+my_profile = {
+    "name" => "Ben",
+    "location"=> {"city" => "Chicago", "state" => "Illinois"},
+    "status" => "Kellogg"
+}
+puts my_profile
+
+puts my_profile["location"]["city"]
+p my_profile["location"]["city"]
+
+my_profile["name"] = "Nathan Jones"
+puts my_profile
+
